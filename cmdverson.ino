252b99751e0f7dfa20667bd011c27e6f16af5f4f
Pramish Bhusal <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-07-04 21:53:18 +0545

    Add ESP32 robot control via web server

---
+++ cmdverson.ino
@@ -0,0 +1,115 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "YOUR_HOTSPOT_NAME";
+const char* password = "YOUR_PASSWORD";
+
+WebServer server(80);
+
+#define IN1 27
+#define IN2 26
+#define IN3 25
+#define IN4 33
+
+String cmd = "stop";
+
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void handleRoot() {
+  server.send(200, "text/html", R"rawliteral(
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Robot</title>
+<style>
+button { width:100%; height:30vh; font-size:20px; }
+</style>
+</head>
+<body>
+
+<h2>ESP32 Robot</h2>
+
+<button onclick="send('left')">Left</button><br>
+<button onclick="send('back')">Back</button><br>
+<button onclick="send('right')">Right</button><br>
+
+<script>
+function send(c){
+  fetch("/cmd?val=" + c);
+}
+</script>
+
+</body>
+</html>
+
+)rawliteral");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  stopMotor();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+
+  server.on("/cmd", []() {
+    cmd = server.arg("val");
+
+    if (cmd == "left") left();
+    else if (cmd == "right") right();
+    else if (cmd == "back") backward();
+    else stopMotor();
+
+    server.send(200, "text/plain", "OK");
+  });
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
